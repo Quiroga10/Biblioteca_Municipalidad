df94719ec06a5ec9028e8f8afff351bd4769f883
Junior Quiroga <juniorquirogamio@gmail.com>
2023-10-28 14:49:15 -0500

    script google maps

--- js/google_maps.js
+++ js/google_maps.js
@@ -1,17 +1,14 @@
+// Función para inicializar el mapa
 function initMap() {
-    // Coordenadas de la Municipalidad Provincial de Lambayeque
-    var ubicacion = { lat: -6.701084, lng: -79.901313 };
+    const location = { lat: -6.803707, lng: -79.838209 }; // Coordenadas de la ubicación de la institución educativa
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 15,
+        center: location,
+});
 
-    // Crea un nuevo mapa y lo coloca en el elemento con id "map"
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: ubicacion,
-        zoom: 15 // Ajusta el nivel de zoom según tus preferencias
-    });
-
-    // Crea un marcador en la ubicación
-    var marker = new google.maps.Marker({
-        position: ubicacion,
-        map: map,
-        title: 'Municipalidad Provincial de Lambayeque'
-    });
+const marker = new google.maps.Marker({
+    position: location,
+    map: map,
+    title: "Institución Educativa Carmelo Felix Medrano 10125 - Jayanca",
+});
 }
