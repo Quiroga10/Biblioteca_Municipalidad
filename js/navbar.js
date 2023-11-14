2e62532561d2ab73b2e1d409c0f35d1e9daab693
Junior Quiroga <juniorquirogamio@gmail.com>
2023-11-14 03:14:13 -0500

    scroll

--- js/navbar.js
+++ js/navbar.js
@@ -26,3 +26,36 @@ document.getElementById('close-menu').addEventListener('click', function() {
         body.style.overflow = 'auto';
     }, 300); // 300ms es la duración de la transición CSS
 });
+
+//desplazamiento cuando no se visualice la etiqueta header
+document.addEventListener("DOMContentLoaded", function () {
+    // Obtén el elemento header
+    var header = document.querySelector("header");
+    
+    // Obtén el elemento secciones-menu
+    var seccionesMenu = document.getElementById("secciones-menu");
+
+    // Función para verificar el desplazamiento y aplicar el estilo
+    function checkScroll() {
+        // Obtén la altura del header
+        var headerHeight = header.clientHeight;
+
+        // Obtén la posición actual de desplazamiento
+        var scrollPosition = window.scrollY || window.pageYOffset;
+
+        // Verifica si el header ya no es visible
+        if (scrollPosition > headerHeight) {
+            // Aplica un nuevo estilo al secciones-menu
+            seccionesMenu.style.marginTop = "10px"; // Ajusta según sea necesario
+        } else {
+            // Vuelve al estilo original si el header es visible
+            seccionesMenu.style.marginTop = "23rem"; // Ajusta según sea necesario
+        }
+    }
+
+    // Agrega un evento de desplazamiento para llamar a la función
+    window.addEventListener("scroll", checkScroll);
+
+    // Llama a la función una vez al cargar la página
+    checkScroll();
+});
